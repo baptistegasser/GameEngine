0a813de7a0023b702516dee8595e117e481a9a47
Baptiste Gasser <baptiste.gasser@etu.univ-amu.fr>
2021-11-20 23:18:29 -0500

    Utility to convert string to wide string and vice versa (unsafe)

--- Projet/util/Util.cpp
+++ Projet/util/Util.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "Util.h"
+
+const wchar_t* str2whchar(const std::string& str) noexcept
+{
+	return std::wstring{ str.begin(), str.end() }.c_str();
+}
+
+const std::string whchar2str(const wchar_t* str) noexcept
+{
+	const auto wstr = std::wstring(str);
+// Ignore this warning we known that converting a wide string to simple string is unsafe, that's the whole purpose of doing it
+#pragma warning(suppress: 4244)
+	return std::string{ wstr.begin(), wstr.end() };
+}
--- Projet/util/Util.h
+++ Projet/util/Util.h
@@ -1,4 +1,9 @@
 #pragma once
 
+#include <string>
+
 // Physx resources cleaning
 #define PX_RELEASE(res) if(res) { res->release(); res = nullptr; }
+
+const wchar_t* str2whchar(const std::string & str) noexcept;
+const std::string whchar2str(const wchar_t* str) noexcept;
